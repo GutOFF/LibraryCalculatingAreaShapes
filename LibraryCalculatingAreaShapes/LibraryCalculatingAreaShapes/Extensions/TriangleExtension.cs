7554247f6891947a4408f1766c390f1fda277d5c
Andrew Gutov <gyhadyt@mail.ru>
2024-03-07 01:02:05 +0500

    Добавлен метод расширения для проверки правильности

---
+++ LibraryCalculatingAreaShapes/LibraryCalculatingAreaShapes/Extensions/TriangleExtension.cs
@@ -0,0 +1,14 @@
+﻿using LibraryCalculatingAreaShapes.Figures;
+
+namespace LibraryCalculatingAreaShapes.Extensions;
+
+public static class TriangleExtension
+{
+    public static bool IsRightTriangle(this Triangle triangle)
+    {
+        double[] sides = { triangle.Side1, triangle.Side2, triangle.Side3 };
+        Array.Sort(sides);
+
+        return Math.Abs(Math.Pow(sides[2], 2) - (Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2))) < 0.0001;
+    }
+}
